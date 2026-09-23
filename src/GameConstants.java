ee6765ca3ba529b2c029fe74741004f1d5e3d3cb
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-09-23 10:56:59 -0300

    VERSAO5

---
+++ src/GameConstants.java
@@ -0,0 +1,9 @@
+    public final class GameConstants {
+        private GameConstants() {
+
+        }
+        public static final String GAME_TITLE = "Snake Game";
+
+        public static final int TILE = 30;
+        Public static final int 
+    }
